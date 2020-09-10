252d57ea431bd7026b1acce1469bd75b83a3196a
Artemis <wzyzhouyi@gmail.com>
2020-09-09 17:09:52 -0700

    added limitation that user has to choose at least one type of character

--- Develop/script.js
+++ Develop/script.js
@@ -2,29 +2,36 @@
 var generateBtn = document.querySelector("#generate");
 
 var lengthPassword = 0;
-var isLowerCase = true;
-var isUpperCase = true;
-var isNumeric = true;
-var isSpecialChar = true;
+var isLowerCase = false;
+var isUpperCase = false;
+var isNumeric = false;
+var isSpecialChar = false;
 
-// Prompt criteria to user
+// Prompt criteria for user selection
 function passwordCriteria() {
+  
   var resultListCriteria = false;
   var firstMessage = "Please select from the following criteria to include in password: \n-Length between 8-128 characters\n-Lowercase\n-Uppercase\n-Numeric\n-Special characters";
 
+  // First message to user with a list of password criteria
   while (resultListCriteria === false) {
     resultListCriteria = confirm(firstMessage);
   }
 
-  lengthPassword = prompt("Please enter length of password (must be between 8 and 128 characters)");
+  // Take user input of password length
+  lengthPassword = prompt("Please enter length of password (must be between 8 and 128 characters).");
   while (lengthPassword > 128 || lengthPassword < 8) {
     lengthPassword = prompt("Password length must be between 8 and 128 characters. Please enter again.");
   }
 
-  // isLowerCase = confirm("Lowercase");
-  // isUpperCase = confirm("Uppercase");
-  // isNumeric = confirm("Numeric");
-  // isSpecialChar = confirm("Special Character");
+  // Take user choice of character type
+  while ((isLowerCase === false) && (isUpperCase === false) && (isNumeric === false) && (isSpecialChar === false)) {
+    alert("Please choose at least one type of character.");
+    isLowerCase = confirm("Lowercase");
+    isUpperCase = confirm("Uppercase");
+    isNumeric = confirm("Numeric");
+    isSpecialChar = confirm("Special Character");
+  }
 
 }
 
